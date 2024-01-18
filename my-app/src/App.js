10d1f7730d4198298f9cb2fd2da893f4cab524f4
unknown <30009351@students.ual.pt>
2024-01-18 10:33:09 +0000

    quase la

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,46 @@
-"use client";
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import {
   APIProvider,
+  Marker,
   Map,
 } from "@vis.gl/react-google-maps";
 
 const App = () => {
-  const position = { lat: 38.7245447, lng: -9.1457366};
+
+  const [newAd, setAd] = useState({lat: 38.7245447, lng: -9.1457366})
+  const [newinput, setinput] = useState("amora,lisboa")
+
+  const ChangeAd = (event) => {
+    event.preventDefault()
+
+    axios
+      .get('https://maps.googleapis.com/maps/api/geocode/json?address='+newinput.replace(" ", "%")+'&key=AIzaSyD7tIANYEbsVlVcCG4QWrwMs6xyoXmi1sk')
+      .then(response => {
+        setAd(response.data.results[0].geometry.location)
+    })
+
+  }
+
+  const handleNoteChange = (event) => {
+    event.preventDefault()
+    setinput(event.target.value)
+  }
 
   return (
     <APIProvider apiKey={'AIzaSyD7tIANYEbsVlVcCG4QWrwMs6xyoXmi1sk'}>
-    
-      <div>
-        <h4>Event locations in your area</h4>
-      </div>
-      <div class="my-2">
-        <form>
-          <label for="zip">Search by city/address:</label>
-          
-          <button type="submit" value="Encode" id="button" onclick="handleSubmit(event)" class="btn btn-secondary">Search</button>
-        </form>
-      </div>
 
       <div style={{ height: "50vh", width: "50%" }}>
-        <Map zoom={15} center={position}>
-
+        <h4>Event locations in your area</h4>
+        <form onSubmit={ChangeAd}>
+          <input
+            onChange={handleNoteChange}
+          />
+          <button type="submit">save</button>
+        </form> 
+        <Map zoom={15} center={newAd}>
+        < Marker position={newAd} />
         </Map>
       </div>
       
